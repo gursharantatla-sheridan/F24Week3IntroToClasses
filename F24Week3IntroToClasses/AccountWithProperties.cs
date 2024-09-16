f050eb5e8d82d8dd78d9245348070c281e032fa0
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-16 09:02:11 -0400

    account with properties

---
+++ F24Week3IntroToClasses/AccountWithProperties.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F24Week3IntroToClasses
+{
+    public class AccountWithProperties
+    {
+		// field
+		private double _balance;
+
+		// property
+		public double Balance
+		{
+			get { return _balance; }
+			set
+			{ 
+				if (value >= 0)
+					_balance = value; 
+			}
+		}
+
+		// constructor
+		public AccountWithProperties(double initialBalance)
+		{
+			Balance = initialBalance;
+		}
+
+        // methods
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > Balance)
+                throw new Exception("Amount cannot exceed the balance");
+
+            Balance -= amount;
+        }
+    }
+}
