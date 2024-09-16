020dbf0dfad15afa67ab022a8f35b4f4f428d2ed
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-16 08:25:12 -0400

    account with getters and setters

---
+++ F24Week3IntroToClasses/AccountWithMethods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F24Week3IntroToClasses
+{
+    public class AccountWithMethods
+    {
+        // field
+        private double _balance;
+
+        // constructor
+        public AccountWithMethods(double initialBalance)
+        {
+            _balance = initialBalance;
+        }
+
+        // getters / setters
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+        public void SetBalance(double amount)
+        {
+            _balance = amount;
+        }
+
+        // methods
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            _balance -= amount;
+        }
+    }
+}
